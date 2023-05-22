ca9f922e32bbfba3d6487431d9a5f0987f6618fb
bmaxdk <choh8uw@gmail.com>
2023-05-22 00:37:22 -0400

    Uploaded Src

---
+++ src/scripts/test_navigation.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ws="/home/workspace/catkin_ws"
+
+xterm -e "cd ${ws}; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${ws}/src/map/cho_world.world" &
+sleep 6
+
+xterm -e "cd ${ws} && source devel/setup.bash && roslaunch turtlebot_gazebo amcl_demo.launch map_file:=${ws}/src/map/map.yaml" &
+sleep 6
+
+xterm -e "cd ${ws} && source devel/setup.bash && roslaunch turtlebot_rviz_launchers view_navigation.launch"
--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-xterm -e "cd ../..; source/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/worlds/corridor.world" &
+# source /opt/ros/kinetic/setup.bash
+# source devel/setup.bash
 
+ws=/home/workspace/catkin_ws/
+# xterm -e "cd ${ws}; source/devel/setub.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/worlds/playground.world" &
+xterm -e "cd ${ws}; source/devel/setub.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${ws}/src/map/cho_world.world" &
+# xterm -e "cd ${ws}; source/devel/setub.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/turtlebot_simulator/turtlebot_gazebo/worlds/cho_world.world" &
+# xterm -e "cd ${ws}; source/devel/setub.bash; roslaunch turtlebot_gazebo turtlebot_world.launch"&
 sleep 5
 
-xterm -e "cd ../..; source/devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm -e "cd ${ws}; source/devel/setub.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
 
-xterm -e "cd ../..; source/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "cd ${ws}; source/devel/setub.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 
-xterm -e "cd ../..; source/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
+xterm -e "cd ${ws}; source/devel/setub.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
