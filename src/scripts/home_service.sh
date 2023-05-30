2c2e70c1579978645427a0cfdd4df593636d004a
bmaxdk <choh8uw@gmail.com>
2023-05-29 20:11:20 -0400

    Upload home_servic.sh && rviz_marker

---
+++ src/scripts/home_service.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+ws="/home/workspace/catkin_ws"
+
+xterm -e "cd ${ws} && source devel/setup.bash && roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${ws}/src/map/cho_world.world" &
+sleep 6
+
+xterm -e "cd ${ws} && source devel/setup.bash && roslaunch turtlebot_gazebo amcl_demo.launch map_file:=${ws}/src/map/map.yaml" &
+sleep 6
+
+xterm -e "cd ${ws} && source devel/setup.bash && roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 6
+
+xterm -e "cd ${ws} && source devel/setup.bash && rosrun pick_objects pick_objects" &
+sleep 8
+
+xterm -e "cd ${ws} && source devel/setup.bash && rosrun add_markers add_markers"
